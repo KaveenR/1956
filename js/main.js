e05cc8f75c29e230539afbc195b49935ff2ee5f0
Kaveen Rodrigo <u.k.k.rodirgo@gmail.com>
2016-07-23 08:16:33 +0530

    basic implementation

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var app = angular.module('ninteenfiftysix',[]);
+var COL_NAMES = ['1','5','6','9'];
+
+app.controller('mainCtrl', function($scope, $http, $rootScope) {
+  $scope.data = [];
+  $scope.dict = [];
+  $scope.dict_keys = [];
+  $scope.ready = false;
+
+  $scope.init = function(){
+    console.log("YOLO");
+    $http.get("data.json").success(function(dat){
+      $scope.data = dat;
+      $scope.ready = true;
+      $scope.dict = $scope.makeDict($scope.data);
+      $scope.dict_keys = Object.keys($scope.dict);
+      console.log($scope.dict);
+    }).error(function(){
+      alert("Network Error");
+    });
+  }
+
+  $scope.resolve = function(key){
+    var cur = $scope.dict[key];
+    var rands = Math.floor((Math.random() * cur.length) + 0)
+    var current_entry = $scope.data[cur[rands]];
+    alert($scope.entryTOS(current_entry));
+  }
+
+  $scope.entryTOS = function(entry){
+    var str = "";
+    for ( i in COL_NAMES){
+      str = str + " " + entry[COL_NAMES[i]];
+    }
+    return str;
+  }
+  $scope.makeDict = function(data){
+    var dict = [];
+    for(var row in data){
+      for(var col in COL_NAMES){
+        var cur_word = data[row][COL_NAMES[col]];
+        if (dict[cur_word] == undefined){
+          dict[cur_word] = [parseInt(row)];
+        }else{
+          dict[cur_word].push(parseInt(row));
+        }
+      }
+    }
+    return dict;
+  }
+
+});
